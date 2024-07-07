43d3a34db3391cd92300ad234425b347e3d38631
Iryna <postika.iryna@gmail.com>
2024-07-07 14:13:08 +0000

    updated docker_container resource

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-  name = "zyavrusha/jenkins_task3:60" # define the build which is used to run container
+  name = "zyavrusha/nginx_app_task3:60" # define the build which is used to run container
 }
 
 resource "docker_container" "simple_app" {
   count = 3 # Create 3 containers 
 
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.name
   name  = "nginx-container-${count.index}"
   ports {
 	internal = 80
